5e1a6e0a748b388d89cb0273d545fee7ee1fbb37
andres-altena <76436627+andres-altena@users.noreply.github.com>
2021-02-11 10:23:49 +0000

    Make OOP based

---
+++ source/Preprocessor.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[22]:
+
+
+from Tokeniser import TextTokeniser
+from Decoder import Decoder
+
+class Preprocessor():
+    #Preprocessor object will deal with 
+    #1st Preprocessing data
+    #2nd Tokenising data
+    #3rd Preprocessing tokenised data
+    
+    def __init__(self):
+        self.tokeniser = TextTokeniser()
+        self.decoder = Decoder()
+    
+    def pre_token_preprocessing(self, text):
+        text = self.decoder.decode(text)
+        return text
+    
+    def post_token_preprocessing(self, text):
+        return text
+    
+    def preprocess_text(self, text):
+        #initial preprocessing
+        text = self.pre_token_preprocessing(text)
+        #Tokenise the text
+        text = self.tokeniser.clean_and_tokenise_text(text)
+        #final preprocessing
+        text = self.post_token_preprocessing(text)
+        
+        return text
+
